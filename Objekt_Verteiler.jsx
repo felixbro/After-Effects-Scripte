b8eb930ddd0b57788f739b9d4ed9e912221a7fc1
Felix Broschat <64419976+felixbro@users.noreply.github.com>
2025-10-06 09:41:44 +0200

    Update and rename Verteiler.jsx to Objekt_Verteiler.jsx
    Komplett neu geschrieben. Alte Version funktionierte nicht. Diese schon

---
+++ Objekt_Verteiler.jsx
@@ -0,0 +1,35 @@
+(function distributeBetweenLayers() {
+    var comp = app.project.activeItem;
+    if (!comp || !(comp instanceof CompItem)) {
+        alert("Bitte öffne eine Komposition und wähle mindestens 3 Layer aus.");
+        return;
+    }
+
+    var selectedLayers = comp.selectedLayers;
+    var count = selectedLayers.length;
+
+    if (count < 3) {
+        alert("Bitte wähle mindestens 3 Layer aus.");
+        return;
+    }
+
+    app.beginUndoGroup("Objekte gleichmäßig verteilen");
+
+    var firstLayer = selectedLayers[0];
+    var lastLayer = selectedLayers[count - 1];
+
+    for (var i = 1; i < count - 1; i++) {
+        var currentLayer = selectedLayers[i];
+
+        // Expression string zusammensetzen
+        var expression = 
+            "var obj1 = thisComp.layer('" + firstLayer.name + "');" +
+            "var obj2 = thisComp.layer('" + lastLayer.name + "');" +
+            "obj1.transform.position + (obj2.transform.position - obj1.transform.position) / " + (count - 1) + " * " + i + ";";
+
+        currentLayer.transform.position.expression = expression;
+    }
+
+    app.endUndoGroup();
+    alert((count - 2) + " Layer dynamisch verteilt.");
+})();
